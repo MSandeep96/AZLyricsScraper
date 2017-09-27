158f19418ecc1399e4bf7b763de5bdae208906b6
Sandeep Reddy <msande1996@gmail.com>
2017-09-27 22:28:02 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+// edit details in the json file
+var artist = require('./artist.json');
+
+var url = 'https://www.azlyrics.com/';
+url += artist.name[0] + '/';
+url += artist.name + '.html';
+// since url is of the form '/e/eminem.html'
+
+
